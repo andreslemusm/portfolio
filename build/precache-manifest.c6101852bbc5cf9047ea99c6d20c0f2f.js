beea65fb2f2b73aba88e69c2022cb02843722adc
Andres Lemus <aclemusm@unal.edu.co>
2020-04-07 13:01:18 -0500

    Add production build

---
+++ build/precache-manifest.c6101852bbc5cf9047ea99c6d20c0f2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9fd16b02ac98693cb39901814639764",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e89887048c3c9201c41",
+    "url": "/static/css/main.259f1213.chunk.css"
+  },
+  {
+    "revision": "872a1936e88855c5ee68",
+    "url": "/static/js/2.89c51293.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.89c51293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e89887048c3c9201c41",
+    "url": "/static/js/main.91ed53fb.chunk.js"
+  },
+  {
+    "revision": "4c15ceb5499f6abee85b",
+    "url": "/static/js/runtime-main.eb07c764.js"
+  },
+  {
+    "revision": "9d2de2565eb00feed508d862ee244e10",
+    "url": "/static/media/LEMUSMADRIDac_1152223198_hojaDeVida.9d2de256.pdf"
+  }
+]);
