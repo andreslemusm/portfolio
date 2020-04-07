e707705306a6074290fe07998252bb6c289dd73d
Andres Lemus <aclemusm@unal.edu.co>
2020-04-07 14:00:32 -0500

    Updates

---
+++ precache-manifest.f787e53d8ce235f4f1140ec9cd326077.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5205efa04aeb95652748de6d633aeae2",
+    "url": "/portfolio/about/index.html"
+  },
+  {
+    "revision": "13e54276d293b080a5ee",
+    "url": "/portfolio/about/static/css/main.259f1213.chunk.css"
+  },
+  {
+    "revision": "872a1936e88855c5ee68",
+    "url": "/portfolio/about/static/js/2.89c51293.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/about/static/js/2.89c51293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13e54276d293b080a5ee",
+    "url": "/portfolio/about/static/js/main.480edb6d.chunk.js"
+  },
+  {
+    "revision": "6926ab8ee9fe5267b899",
+    "url": "/portfolio/about/static/js/runtime-main.d4f880fe.js"
+  },
+  {
+    "revision": "9d2de2565eb00feed508d862ee244e10",
+    "url": "/portfolio/about/static/media/LEMUSMADRIDac_1152223198_hojaDeVida.9d2de256.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.d777c738a69e85b91274263cd6cce977.js"
+  "/portfolio/about/precache-manifest.f787e53d8ce235f4f1140ec9cd326077.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/about/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
