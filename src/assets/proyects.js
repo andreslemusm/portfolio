41c963f3a99a0354743c61f623250fb90e1c095c
Andres Lemus <aclemusm@unal.edu.co>
2020-04-15 00:06:28 -0500

    Add real-estate project

--- src/assets/proyects.js
+++ src/assets/proyects.js
@@ -1,20 +1,22 @@
 const proyectsData = [
   {
-    name: 'Weather App',
-    img: 'https://via.placeholder.com/700x400',
-    technologies: ['React', 'Redux', 'HTML5', 'SASS', 'CSS3'],
-    about: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui nostrum laboriosam debitis quam tempora aliquam, rerum officiis dignissimos velit cum ab iure expedita necessitatibus sint beatae. Amet perferendis corrupti repellendus.
-      Qui quo amet inventore perferendis ducimus libero molestias recusandae praesentium vitae nam. Optio nesciunt alias quisquam omnis assumenda sequi hic delectus nam laboriosam. Velit ut voluptatibus, deleniti dolore commodi nemo.
-      Tempore illum eum at minus libero nesciunt itaque ut quisquam veniam! Excepturi architecto quasi praesentium maiores possimus a ipsam, eius labore eligendi sint nemo, reprehenderit dicta beatae at fugit laborum!
-      Fugit similique, quibusdam magni numquam cupiditate fuga voluptatum veniam libero ratione nam, quod cumque aperiam, modi beatae eum est aut accusamus veritatis ipsa corrupti. At cum voluptatem autem. Nihil, saepe!
-      Porro, modi voluptates similique corporis, ex quis, tempore dolore dignissimos cum ipsam perferendis amet natus omnis exercitationem saepe culpa nesciunt. Officiis, enim totam placeat nesciunt tenetur id porro optio eum.`,
-    demoUrl: 'https://github.com/andresclm',
-    codeUrl: 'https://github.com/andresclm',
+    name: 'Andrew Hill',
+    img: require('./project-images/andrew-hill.png'),
+    technologies: ['HTML', 'PostCSS', 'SASS', 'JavaScript', 'BEM'],
+    about: `The main reason of this project was to reinforce my vanilla habilities, before jumping entirely into React and its wonderful enviroment.
+    
+    I tried to limit myself to just use the old basics HTML, CSS and JavaScript and added some challenges like use the Google Maps API, make a multipage website and use tools like PostCSS to add browser compatibily and SASS to develop faster. 
+    
+    I used the BEM methodology in case my future me wants to reimplement the project in React (Not a fan of rewriting CSS).
+    
+    Last thing and most important since I'm not a designer, this design was inspired by the TAKT Studio website.`,
+    demoUrl: 'https://andresclm.github.io/real-estate/index.html',
+    codeUrl: 'https://github.com/andresclm/real-estate',
   },
   {
     name: 'Films',
     img: 'https://via.placeholder.com/700x400',
-    technologies: ['React', 'Redux', 'HTML5', 'SASS', 'CSS3'],
+    technologies: ['HTML', 'SASS', 'JavaScript'],
     about: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui nostrum laboriosam debitis quam tempora aliquam, rerum officiis dignissimos velit cum ab iure expedita necessitatibus sint beatae. Amet perferendis corrupti repellendus.
       Qui quo amet inventore perferendis ducimus libero molestias recusandae praesentium vitae nam. Optio nesciunt alias quisquam omnis assumenda sequi hic delectus nam laboriosam. Velit ut voluptatibus, deleniti dolore commodi nemo.
       Tempore illum eum at minus libero nesciunt itaque ut quisquam veniam! Excepturi architecto quasi praesentium maiores possimus a ipsam, eius labore eligendi sint nemo, reprehenderit dicta beatae at fugit laborum!
