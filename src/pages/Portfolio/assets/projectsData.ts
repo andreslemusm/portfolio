452d0d9f896ed5988086a39dd38137f458f66e93
Andres Lemus <aclmadrid04@gmail.com>
2020-08-15 13:57:47 -0500

    feat(front): #0 improve projects description

--- src/pages/Portfolio/assets/projectsData.ts
+++ src/pages/Portfolio/assets/projectsData.ts
@@ -7,32 +7,25 @@ export type IProject = {
   codeUrl: string;
 };
 
-export const projectsData: ReadonlyArray<IProject> = [
-  {
-    name: "Andrew Hill",
-    img: require("./projectImages/andrew-hill.jpg"),
-    technologies: ["HTML", "PostCSS", "SASS", "JavaScript", "BEM"],
-    about: `The main reason of this project was to reinforce my vanilla habilities, before jumping entirely into React and its wonderful enviroment.
-
-    I tried to limit myself to just use the old basics HTML, CSS and JavaScript and added some challenges like use the Google Maps API, make a multipage website and use tools like PostCSS to add browser compatibility and SASS to develop faster.
-
-    I used the BEM methodology in case my future me wants to reimplement the project in React (Not a fan of rewriting CSS).
-
-    Last thing and most important since I'm not a designer, this design was inspired by the TAKT Studio website.`,
-    demoUrl: "https://andresclm.github.io/real-estate/index.html",
-    codeUrl: "https://github.com/andresclm/real-estate",
-  },
+export const projectsData: readonly IProject[] = [
   {
     name: "MR NEWS",
     img: require("./projectImages/mr-news.png"),
-    technologies: ["HTML", "CSS", "Tachyons", "JavaScript", "React", "Redux"],
-    about: `This was my second block Acámica project, they gave us a checklist and like any real project, you would have to send it back acording to that checklist.
+    technologies: ["HTML & CSS", "Tachyons", "JavaScript", "React", "Redux"],
+    about: `Mr News is a website where you can read about what is happening around the world. By making use of the New York Times API, we can show you the latest news, from the best writers in the world.
 
-    The three most important topics that I learned were:
-      - The advantages of using a functional CSS framework, like the easiness of adding new features and modificate existence ones.
-      - How to handle multiple request at the same time, by using the Fetch API and learning about Promises.
-      - How the redux cycle works, and how middlewares allow us to make that cycle asynchronous.`,
+    For this project I worked with things like The New York Times API, UI & UX, and I used React and Redux to make the project a Single Page Application.`,
     demoUrl: "https://andresclm.github.io/news-room/",
     codeUrl: "https://github.com/andresclm/news-room/",
+  },
+  {
+    name: "Andrew Hill",
+    img: require("./projectImages/andrew-hill.jpg"),
+    technologies: ["HTML & CSS", "BEM", "SASS", "PostCSS", "JavaScript"],
+    about: `This is a landing page for one of the best neighborhoods in Sydney, Australia. Customers come and see what sets Andrew Hill apart from the competition and then, they can easily continue the home buying process with just two clicks away.
+
+    This is a Multi Page Application and is built using plain HTLM and JavaScript, along with SASS and the BEM methodology.`,
+    demoUrl: "https://andresclm.github.io/real-estate/index.html",
+    codeUrl: "https://github.com/andresclm/real-estate",
   },
 ];
