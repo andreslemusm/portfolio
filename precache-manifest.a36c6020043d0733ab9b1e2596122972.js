2d6c64ae13f8889a1aa6453e76b33d7670fbf223
Andres Lemus <aclmadrid04@gmail.com>
2020-12-02 11:38:06 -0500

    Updates

--- precache-manifest.a36c6020043d0733ab9b1e2596122972.js
+++ precache-manifest.a36c6020043d0733ab9b1e2596122972.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "22a04924df735b714e10d8eb981e7bd3",
+    "revision": "183146231980bb9f3e4a433c1587933b",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "7d81362ec5dcfbfcc691",
-    "url": "/portfolio/static/css/main.8ca9a8a3.chunk.css"
+    "revision": "d31e84e38d05669ce947",
+    "url": "/portfolio/static/css/main.6e101ae1.chunk.css"
   },
   {
     "revision": "944cd8a0330190f4f847",
@@ -16,16 +16,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.7d181888.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7d81362ec5dcfbfcc691",
-    "url": "/portfolio/static/js/main.baeee59e.chunk.js"
+    "revision": "d31e84e38d05669ce947",
+    "url": "/portfolio/static/js/main.6cf279b1.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
     "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
   },
   {
-    "revision": "bcb3db840d958d9df157d2448cef13c9",
-    "url": "/portfolio/static/media/LEMUSMADRIDac_CV.bcb3db84.pdf"
+    "revision": "a1045b7f45d5941b47428bea43934a39",
+    "url": "/portfolio/static/media/LEMUSMADRIDac_CV.a1045b7f.pdf"
   },
   {
     "revision": "3dded3cd5455494fb37387a2b07efef8",
@@ -38,9 +38,5 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "3271b81f37231a48db77d4d500e3d0d7",
     "url": "/portfolio/static/media/mr-news.3271b81f.png"
-  },
-  {
-    "revision": "5796e4fb6126793008997701891c4d4f",
-    "url": "/portfolio/static/media/rmdb.5796e4fb.png"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.bf7313e38fda0e6381b39255b7cb0f85.js"
+  "/portfolio/precache-manifest.a36c6020043d0733ab9b1e2596122972.js"
 );
 
 self.addEventListener('message', (event) => {
