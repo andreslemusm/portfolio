aacc7a18e42d5622eb1214e2cc316e682e198bac
Andres Lemus <aclmadrid04@gmail.com>
2020-12-25 10:46:24 -0500

    Updates

---
+++ precache-manifest.abf1e21182699d0c13fb3d4c6e990ca1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b9d1cb14d65d2620833ff5f90d06e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcb36d2e00c8adde8224",
+    "url": "/static/css/main.eafb3896.chunk.css"
+  },
+  {
+    "revision": "cd0a18cf17b52e5cdd2d",
+    "url": "/static/js/2.2aaaf894.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2aaaf894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcb36d2e00c8adde8224",
+    "url": "/static/js/main.388dcd7f.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "a1045b7f45d5941b47428bea43934a39",
+    "url": "/static/media/LEMUSMADRIDac_CV.a1045b7f.pdf"
+  },
+  {
+    "revision": "3dded3cd5455494fb37387a2b07efef8",
+    "url": "/static/media/aclm.3dded3cd.svg"
+  },
+  {
+    "revision": "b55e8692fb31335ee3a9afdb6aa68b92",
+    "url": "/static/media/andrew-hill.b55e8692.jpg"
+  },
+  {
+    "revision": "3271b81f37231a48db77d4d500e3d0d7",
+    "url": "/static/media/mr-news.3271b81f.png"
+  },
+  {
+    "revision": "5796e4fb6126793008997701891c4d4f",
+    "url": "/static/media/rmdb.5796e4fb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a577672ef28239cfe8bce4f36761f3c5.js"
+  "/precache-manifest.abf1e21182699d0c13fb3d4c6e990ca1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
